0f79ea811657cdc5e0ef80f1a7054e6a8807e5db
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2024-12-01 02:12:24 +0530

    Time: 1 ms (66.64%) | Memory: 46.7 MB (8.17%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -2,21 +2,43 @@ class Solution {
 
     public int removeDuplicates(int[] nums) {
 
-        if (nums.length == 0)
+// M-1
+        // if (nums.length == 0)
+        //     return 0;
+
+        // // `k` is the index for the unique elements
+        // int k = 0;
+
+        // for (int i = 1; i < nums.length; i++) {
+        //     // If the current element is different from the last unique element
+        //     if (nums[i] != nums[k]) {
+        //         k++;
+        //         nums[k] = nums[i];
+        //     }
+        // }
+
+        // return k + 1;
+
+        int ans = recursiveApproach(nums,0,0);
+        return ans;
+
+    }
+
+// M-2
+    public int recursiveApproach(int[] nums, int index, int count) {
+        if (nums.length == 0) {
             return 0;
-
-        // `k` is the index for the unique elements
-        int k = 0;
-
-        for (int i = 1; i < nums.length; i++) {
-            // If the current element is different from the last unique element
-            if (nums[i] != nums[k]) {
-                k++;
-                nums[k] = nums[i];
-            }
+        }
+        if (index >= nums.length) {
+//            nums[count] = nums[nums.length-1];
+            return count;
+        }
+        if (count == 0 || nums[index] != nums[count - 1]) {
+            nums[count] = nums[index];
+            return recursiveApproach(nums, index + 1, count + 1);
         }
 
-        return k + 1;
-
+        // Skip the current element if it's a duplicate
+        return recursiveApproach(nums, index + 1, count);
     }
 }
