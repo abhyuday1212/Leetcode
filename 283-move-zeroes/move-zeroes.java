f4c202f2fe674a940ded537605ed13494b1c7724
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2024-12-30 01:20:42 +0530

    Time: 2 ms (85.05%) | Memory: 46.4 MB (7.34%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,26 +1,57 @@
 class Solution {
+
     public void moveZeroes(int[] nums) {
+        // Optimal
 
-        List<Integer> temp = new ArrayList<>();
-
+        int j = -1;
         int n = nums.length;
 
         for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        if (j == -1)
+            return;
+
+        for (int i = j+1; i < n; i++) {
             if (nums[i] != 0) {
-                temp.add(nums[i]);
+                swap(nums, i, j);
+                j++;
             }
         }
-
-        // Step: 2
-        for (int i = 0; i < n; i++) {
-            if (i >= temp.size()) {
-                nums[i] = 0;
-            }
-            else{
-            nums[i] = temp.get(i);
-            }
-        }
-
 
     }
+
+   public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    // public void moveZeroes(int[] nums) {
+    // // Brute Force
+    // List<Integer> temp = new ArrayList<>();
+
+    // int n = nums.length;
+
+    // for (int i = 0; i < n; i++) {
+    // if (nums[i] != 0) {
+    // temp.add(nums[i]);
+    // }
+    // }
+
+    // // Step: 2
+    // for (int i = 0; i < n; i++) {
+    // if (i >= temp.size()) {
+    // nums[i] = 0;
+    // }
+    // else{
+    // nums[i] = temp.get(i);
+    // }
+    // }
+
+    // }
 }
