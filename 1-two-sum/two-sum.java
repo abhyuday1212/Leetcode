3fdbab6d1890146f54323fac26aa55873b88edf6
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2024-12-31 14:47:17 +0530

    Time: 35 ms (45.95%) | Memory: 45.1 MB (21.91%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,22 +1,28 @@
 class Solution {
- static int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i <= nums.length; i++) {
-            int firstElement = nums[i];
-            for (int j = i+1; j <= nums.length-1; j++) {
-                int seccondElem = nums[j];
-                int sum = seccondElem + firstElement;
-                if (sum == target) {
-//                    finalAns.add(i);
-//                    finalAns.add(j);
-//                    return finalAns;
+    // Brute: Simple For Loop and then adding the element
+    // Better: Hashing (Optimal way if we need to return the index also)
+    
+    // Optimal: 2 Pointer Approach 
+    // Only optimal for Varity 1 problems where we need to return the boolean values as we need to sort the array, and if we sort the array then indexes will get disturb with the end pointer at last and then checking if sum is greater than or less than the current two sum elements
 
-                    return new int[]{i, j};
-                }
+    static int[] twoSum(int[] nums, int target) {
+    for (int i = 0; i <= nums.length; i++) {
+    int firstElement = nums[i];
+    for (int j = i + 1; j <= nums.length - 1; j++) {
+    int seccondElem = nums[j];
+    int sum = seccondElem + firstElement;
+    if (sum == target) {
+    // finalAns.add(i);
+    // finalAns.add(j);
+    // return finalAns;
 
-            }
-        }
+    return new int[] { i, j };
+    }
 
-        return new int[0];
+    }
+    }
+
+    return new int[0];
 
     }
 }
