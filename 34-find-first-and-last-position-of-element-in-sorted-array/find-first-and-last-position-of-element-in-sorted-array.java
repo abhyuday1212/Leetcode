67f9b2d2db4d5abce427e25fe5e596c1d88639f8
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-01-08 22:56:52 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (57.09%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,39 +1,78 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans = { -1, -1 };
 
-        // Check for the first occurence of target;
-        int start = searchTargetIndex(nums, target, true);
-        int end = searchTargetIndex(nums, target, false);
-        ans[0] = start;
-        ans[1] = end;
+        if(nums.length == 0) return new int[]{-1,-1};
 
-        return ans;
+
+        int floorVal = floor(nums, target);
+        int ceilVal = ceil(nums, target);
+
+        return new int[] { ceilVal, floorVal };
     }
 
-    // This function returns the index value of target
-    int searchTargetIndex(int[] nums, int target, boolean findStartIndex) {
-        int ans = -1;
+    public int floor(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
+
+         if (target < nums[0]) {
+            return -1; // No floor exists
+        }
+
+        int floor = -1;
+
+        List<Integer> list1 = new ArrayList<>();
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
 
-            if (target < nums[mid]) {
-                end = mid - 1;
-            } else if (target > nums[mid]) {
+            if(nums[mid] == target){
+                list1.add(nums[mid]);
+            }
+
+            if (nums[mid] <= target) {
+                floor = mid;
                 start = mid + 1;
             } else {
-                // Potential ans found
-                ans = mid;
-                if (findStartIndex) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
+                end = mid - 1;
             }
         }
-        return ans;
+
+        if(list1.isEmpty()) return -1;
+
+        return floor;
+    }
+
+    public int ceil(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        
+
+        if (target > nums[end]) {
+            return -1; // No ceil exists
+        }
+
+        List<Integer> list1 = new ArrayList<>();
+
+        int ceil = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] == target){
+                list1.add(nums[mid]);
+            }
+
+            if (nums[mid] >= target) {
+                ceil = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        if(list1.isEmpty()) return -1;
+
+        return ceil;
     }
 }
