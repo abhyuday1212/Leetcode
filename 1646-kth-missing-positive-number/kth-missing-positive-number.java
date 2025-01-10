f608d46761953ec0dabd08f08c4565442d1850e1
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-01-11 02:26:40 +0530

    Time: 6 ms (4.77%) | Memory: 44.5 MB (5.66%) - LeetSync

---
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int ans = -1;
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for (int num : arr) {
+            mp.put(num, 1); // Mark these numbers as present
+        }
+  
+        List<Integer> emptyList = new ArrayList<>();
+  
+
+        int current = 1;
+
+        while (emptyList.size() < k) {
+            if (!mp.containsKey(current)) {
+                // If the current number is missing, add it to the list
+                emptyList.add(current);
+            }
+            current++;
+        }
+ 
+        return emptyList.get(k-1);
+
+    }
+}
