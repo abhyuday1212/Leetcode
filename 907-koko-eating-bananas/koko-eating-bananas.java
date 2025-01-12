07908f82053412e50d4fd1c4d16589fa72bec0bc
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-01-12 20:11:05 +0530

    Time: 17 ms (57.62%) | Memory: 45.4 MB (21.91%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+        int start = 1;
+
+        int end = piles[piles.length - 1];
+        int ans = 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            int timeTaken = totalTimeWithKSpeed(piles, mid, h);
+
+            // if (timeTaken == 0) return mid;
+
+            if (timeTaken >= 0) { 
+                //speed is greater so reduce the speed and check for the value lesser than this till mid is compensated
+                ans = mid;  //possible Ans
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public int totalTimeWithKSpeed(int[] arr, int tempSpeed, int h) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= tempSpeed) {
+                h = h - 1;
+            } else { //current bananas is greater > than speed
+                int timeRequired = arr[i] / tempSpeed;
+                if (arr[i] % tempSpeed != 0) {
+                    timeRequired += 1; //add extra time for the greater number of banas than tempSpeed
+                }
+                h = h - timeRequired;
+            }
+        }
+
+        return h;
+    }
+}
