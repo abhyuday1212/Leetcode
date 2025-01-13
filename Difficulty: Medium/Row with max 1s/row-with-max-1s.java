8d4b4e0fcd001a6bcd48c8388b9ce1741823414e
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-01-14 01:00:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.java
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt(); // Number of test cases
+        sc.nextLine();        // Consume the newline character
+
+        while (t-- > 0) {
+            String input = sc.nextLine();
+
+            // Replace ][ with ],[
+            input = input.replace("][", "],[");
+
+            ArrayList<ArrayList<Integer>> mat = new ArrayList<>();
+            String[] rows = input.split("],\\s*\\[");
+
+            for (String row : rows) {
+                row = row.replaceAll("[\\[\\]]", ""); // Remove any surrounding brackets
+                ArrayList<Integer> intRow = new ArrayList<>();
+                for (String num : row.split(",")) {
+                    intRow.add(Integer.parseInt(num.trim()));
+                }
+                mat.add(intRow);
+            }
+
+            Solution obj = new Solution();
+            int result = obj.rowWithMax1s(convertListToArray(mat));
+
+            System.out.println(result);
+
+            System.out.println("~");
+        }
+        sc.close();
+    }
+
+    // Helper method to convert ArrayList<ArrayList<Integer>> to 2D array
+    public static int[][] convertListToArray(ArrayList<ArrayList<Integer>> mat) {
+        int rows = mat.size();
+        int cols = mat.get(0).size();
+        int[][] arr = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = mat.get(i).get(j);
+            }
+        }
+        return arr;
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int rowWithMax1s(int arr[][]) {
+        int maxCount = 0;
+        int index = 0;
+        
+        int n = arr.length;
+        
+        for(int i = 0 ; i < n; i++){
+            int m = arr[i].length;
+            
+            int countOne =  m - count1sOfEachRow(arr[i], m , 1);
+            
+            if(maxCount < countOne){
+                index = i;
+                maxCount = countOne;
+            }
+            
+        }
+        
+        return index;
+    }
+    
+    //this will give the number of 1s in the row
+    
+    public int count1sOfEachRow(int[] arr, int m, int x){
+        int start = 0;
+        int end = m - 1;
+        
+        int ans = m;
+        
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            
+            if(arr[mid] >=  x){
+                ans = mid;
+                end = mid  - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+}
