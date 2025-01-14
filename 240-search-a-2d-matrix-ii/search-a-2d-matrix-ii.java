6f7e241f15af517cda92d561511dde2e551029fc
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-01-14 22:05:07 +0530

    Time: 5 ms (99.80%) | Memory: 46 MB (66.04%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    // Optimal Approach: transform the 2d Array such that it is intersecting at the corner point, it is increasing from one end and decreasing on the other end;
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int row = 0;
+        int col = m - 1;
+
+        while(row< n && col >= 0){
+            //matrix[row][col] is the corner most index of the matrix at top right
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] < target) row++; // since element is bigger than move down to search in higher search space
+
+            else col--; //move leftword as we have choosed the matrix[row][col] as the mid element
+        }
+
+        return false;
+    }
+}
