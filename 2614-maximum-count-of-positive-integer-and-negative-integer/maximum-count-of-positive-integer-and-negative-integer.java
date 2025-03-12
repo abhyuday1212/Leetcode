5f33416f0d3ef629148f86b8d70d9ad7e2cd9971
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-03-13 00:28:48 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (25.27%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,25 +1,61 @@
 class Solution {
     public int maximumCount(int[] nums) {
-        int neg = 0;
-        int pos = 0;
+        // approach using binary search
         int n = nums.length;
+        int start = 0;
+        int end = n - 1;
+        int neg = 0;
 
-        int index = 0;
-        while (index < n) {
-            int curr = nums[index];
-            if (curr == 0) {
-                index++;
-                continue;
-            } else if (curr > 0) {
-                break;
+        // find the negative ending index with this loop
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int curr = nums[mid];
+
+            if (curr < 0) {
+                // it is possible that the current index is probable ans, but we will find
+                // better
+                neg = mid + 1; // assign the mid index as one of the possible ans
+                start = mid + 1;
+
+            } else {
+                end = mid - 1;
             }
-            System.out.println(curr);
-            neg++;
+        }
+
+        // the neg index will automatically point to the last negative index
+        int index = neg;
+        while (index < n  && nums[index] == 0) {
             index++;
         }
 
-        pos = n - index;
+        int pos = n - index;
+        int maxAns = Math.max(pos, neg);
 
-        return Math.max(pos, neg);
+        return maxAns;
     }
+
+    // Brut: TC -> O(n)
+    // public int maximumCount(int[] nums) {
+    // int neg = 0;
+    // int pos = 0;
+    // int n = nums.length;
+
+    // int index = 0;
+    // while (index < n) {
+    // int curr = nums[index];
+    // if (curr == 0) {
+    // index++;
+    // continue;
+    // } else if (curr > 0) {
+    // break;
+    // }
+    // System.out.println(curr);
+    // neg++;
+    // index++;
+    // }
+
+    // pos = n - index;
+
+    // return Math.max(pos, neg);
+    // }
 }
