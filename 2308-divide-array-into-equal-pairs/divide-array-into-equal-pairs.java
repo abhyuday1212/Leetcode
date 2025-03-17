5678bf4642a0b41a3943a47cbda6a3e907339dd5
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-03-18 02:45:50 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (58.84%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
@@ -1,13 +1,28 @@
 class Solution {
+    // better
     public boolean divideArray(int[] nums) {
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length; i = i + 2) {
-            if (nums[i] != nums[i + 1]) {
+        int[] freq = new int[501]; // since nums[i] is in range 1 to 500
+        for (int num : nums) {
+            freq[num]++;
+        }
+        for (int count : freq) {
+            if (count % 2 != 0) {
                 return false;
             }
         }
-
         return true;
     }
+
+    // ------ TC: nlogn + n
+    // public boolean divideArray(int[] nums) {
+    //     Arrays.sort(nums);
+
+    //     for (int i = 0; i < nums.length; i = i + 2) {
+    //         if (nums[i] != nums[i + 1]) {
+    //             return false;
+    //         }
+    //     }
+
+    //     return true;
+    // }
 }
