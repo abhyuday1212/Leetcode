1ccf683935abc7dc8d467a6561faa19ae98bc355
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-03-19 09:17:24 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (8.29%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -1,25 +1,61 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> arr = new ArrayList<>();
+        // iterative postorder traversal
+        return iterativePostorder(root);
 
-        postorder(root, arr);
+        // recursive postorder traveral
+        // List<Integer> arr = new ArrayList<>();
 
-        return arr;
+        // postorder(root, arr);
+
+        // return arr;
+    }
+
+    // iterative approach
+    static List<Integer> iterativePostorder(TreeNode root) {
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
+
+        List<Integer> postorder = new ArrayList<>();
+
+        if(root == null) return postorder;
+
+        st1.push(root);
+
+        while (!st1.isEmpty()) {
+            root = st1.pop();
+
+            st2.push(root);
+
+            if (root.left != null) {
+                st1.push(root.left);
+            }
+
+            if (root.right != null) {
+                st1.push(root.right);
+            }
+        }
+
+        while (!st2.isEmpty()) {
+            postorder.add(st2.pop().val);
+        }
+
+        return postorder;
     }
 
     static void postorder(TreeNode root, List<Integer> arr) {
