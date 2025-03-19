c3fa1165629faf8d2ed51cdefad1a605a86e00d3
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-03-20 01:26:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Iterative Postorder/iterative-postorder.java
@@ -0,0 +1,166 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Tree g = new Tree();
+            ArrayList<Integer> res = g.postOrder(root);
+            for (int i = 0; i < res.size(); i++)
+                System.out.print(res.get(i) + " ");
+            System.out.print("\n");
+            t--;
+        
+System.out.println("~");
+}
+    }
+}
+
+// Position this line where user code will be pasted.
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+class Tree {
+    ArrayList<Integer> postOrder(Node node) {
+       return iterativePostorderUsing1Stack(node);
+    }
+    
+    static ArrayList<Integer> iterativePostorderUsing1Stack(Node root) {
+        Stack<Node> st = new Stack<>();
+
+        ArrayList<Integer> postorder = new ArrayList<>();
+
+        if(root == null) return postorder;
+
+        Node curr = root;
+
+        while (curr != null || !st.isEmpty()) {
+            if(curr != null){
+                st.push(curr);
+                curr = curr.left;
+            }
+            else{
+                // this means you are traversing on the right side now
+                Node temp = st.peek();
+                temp = temp.right;
+
+                if(temp == null){
+                    // now get the elements in the stack as that will show the postorder
+                    temp = st.peek();
+                    st.pop();
+
+                    postorder.add(temp.data);
+
+                    while(!st.isEmpty() && temp == st.peek().right){
+                        temp = st.peek();
+                        st.pop();
+                        postorder.add(temp.data); // add the rest of the stack elements to the ans
+                    }
+                }
+                else{
+                    curr = temp;
+                }
+
+            }
+            
+
+        }
+
+        return postorder;
+    }
+}
