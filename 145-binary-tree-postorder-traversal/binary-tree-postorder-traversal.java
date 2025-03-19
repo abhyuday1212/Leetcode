7d37ddf3755429526dfa25c575dad58e846ac67d
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-03-20 01:23:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (65.85%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -15,8 +15,12 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        // iterative postorder traversal
-        return iterativePostorder(root);
+        // iterative postorder using 1 stack traversal
+        return iterativePostorderUsing1Stack(root);
+      
+        // iterative postorder using 1 stack traversal
+        // return iterativePostorder(root);
+
 
         // recursive postorder traveral
         // List<Integer> arr = new ArrayList<>();
@@ -26,7 +30,53 @@ class Solution {
         // return arr;
     }
 
-    // iterative approach
+    // iterative approach using 1 Stack
+    static List<Integer> iterativePostorderUsing1Stack(TreeNode root) {
+        Stack<TreeNode> st = new Stack<>();
+
+        List<Integer> postorder = new ArrayList<>();
+
+        if(root == null) return postorder;
+
+        TreeNode curr = root;
+
+        while (curr != null || !st.isEmpty()) {
+            if(curr != null){
+                st.push(curr);
+                curr = curr.left;
+            }
+            else{
+                // this means you are traversing on the right side now
+                TreeNode temp = st.peek();
+                temp = temp.right;
+
+                if(temp == null){
+                    // now get the elements in the stack as that will show the postorder
+                    temp = st.peek();
+                    st.pop();
+
+                    postorder.add(temp.val);
+
+                    while(!st.isEmpty() && temp == st.peek().right){
+                        temp = st.peek();
+                        st.pop();
+                        postorder.add(temp.val); // add the rest of the stack elements to the ans
+                    }
+                }
+                else{
+                    curr = temp;
+                }
+
+            }
+            
+
+        }
+
+        return postorder;
+    }
+
+
+    // iterative approach using 2 Stack
     static List<Integer> iterativePostorder(TreeNode root) {
         Stack<TreeNode> st1 = new Stack<>();
         Stack<TreeNode> st2 = new Stack<>();
