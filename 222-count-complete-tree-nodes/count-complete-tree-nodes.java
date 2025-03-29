7079ac854e94f63171cd1df7bfe94d5d591fab6b
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-03-30 01:36:35 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (75.90%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -15,11 +15,46 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int leftHt = findLeftHt(root.left);
+        int rightHt = findRightHt(root.right);
+
+        if (leftHt == rightHt){
+            return ((int) Math.pow(2, leftHt + 1) - 1);
+        }
+            
+        return recursivelyTraverse(root);
+    }
+
+    private int findLeftHt(TreeNode root) {
+        int count = 0;
+        while (root != null) {
+            count++;
+            root = root.left;
+        }
+        return count;
+    }
+
+    private int findRightHt(TreeNode root) {
+        int count = 0;
+        while (root != null) {
+            count++;
+            root = root.right;
+        }
+        return count;
+    }
+
+    // Brut:
+    // Tc: O(n)
+    // Sc: O(n)
+    public int recursivelyTraverse(TreeNode root) {
         //inorder traversal
-        if(root == null){
+        if (root == null) {
             return 0;
         }
 
-        return 1 + countNodes(root.left) + countNodes(root.right);
-    } 
+        return 1 + recursivelyTraverse(root.left) + recursivelyTraverse(root.right);
+    }
 }
