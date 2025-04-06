9e5cb912c46d5e4241c5ce2ac41eeff9f25667d6
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-04-07 01:24:10 +0530

    Time: 5 ms (100.00%) | Memory: 45.4 MB (16.80%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,17 +9,41 @@
  */
 
 class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode> path1 = new ArrayList<>();
-        boolean found1 = findPath(root, p, path1);
-        
-        System.out.println("Printing for Q:");
-        List<TreeNode> path2 = new ArrayList<>();
-        boolean found2 = findPath(root, q, path2);
+    // Optimal: 
+    // TC: O(H)
+    // SC: Reccursice stack space
+     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null){
+            return null;
+        }
 
-        //now findLCA
-        return findLCA(path1, path2);
+        int cur = root.val;
+        
+        // if both lie right side
+        if(cur < p.val && cur < q.val){
+            // then move right
+            return lowestCommonAncestor(root.right, p, q);
+        }
+
+        if(cur > p.val && cur > q.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }
+
+        return root;
     }
+
+    // M-1:
+    // public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    //     List<TreeNode> path1 = new ArrayList<>();
+    //     boolean found1 = findPath(root, p, path1);
+        
+    //     System.out.println("Printing for Q:");
+    //     List<TreeNode> path2 = new ArrayList<>();
+    //     boolean found2 = findPath(root, q, path2);
+
+    //     //now findLCA
+    //     return findLCA(path1, path2);
+    // }
 
     public TreeNode findLCA(List<TreeNode> l1, List<TreeNode> l2){
         int size = l1.size() < l2.size() ? l1.size() : l2.size();
