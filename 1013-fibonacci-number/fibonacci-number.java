e6bc8458154fbd58f3a5d7bc8c997da2ef501b00
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-04-23 21:09:36 +0530

    Time: 9 ms (23.51%) | Memory: 40.1 MB (96.47%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,11 +1,10 @@
 class Solution {
     public int fib(int n) {
-                if(n<2){
+        if(n<2){
             return n;
         }
 
         //Linear recursive Relation
         return fib(n-1)+ fib(n-2);
     }
- 
 }
