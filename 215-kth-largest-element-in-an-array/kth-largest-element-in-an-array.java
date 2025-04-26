ffdcaec5598bb55a4d4fabb68220b76121d68e9e
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-04-26 19:22:40 +0530

    Time: 40 ms (71.59%) | Memory: 57.4 MB (82.42%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -7,36 +7,37 @@ class Solution {
     
     Space Complexity: O(n)
      */
-    public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        // Build heap in O(n)
-        maxHeap.addAll(Arrays.stream(nums).boxed().collect(Collectors.toList()));
-        // Extract max k-1 times
-        for (int i = 1; i < k; i++) {
-            maxHeap.poll();
-        }
-        return maxHeap.peek();
-    }
+    // public int findKthLargest(int[] nums, int k) {
+    //     PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+    //     // Build heap in O(n)
+    //     maxHeap.addAll(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+    //     // Extract max k-1 times
+    //     for (int i = 1; i < k; i++) {
+    //         maxHeap.poll();
+    //     }
+    //     return maxHeap.peek();
+    // }
 
     // TC: O(Nlog(k))
     // Using Min-Heap approach
-    // public int findKthLargest(int[] nums, int k) {
-    //     int n = nums.length;
-    //     PriorityQueue<Integer> pq = new PriorityQueue<>(); // Default MinHeap Priority Queue
+    // Faster
+    public int findKthLargest(int[] nums, int k) {
+        int n = nums.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // Default MinHeap Priority Queue
 
-    //     for(int i = 0; i < k; i++){
-    //         pq.add(nums[i]);
-    //     }
+        for(int i = 0; i < k; i++){
+            pq.add(nums[i]);
+        }
 
-    //     for(int i = k; i < n; i++){
-    //         if(pq.peek() < nums[i]){
-    //             // pop the top element and add the current element to it
-    //             pq.poll();
-    //             pq.add(nums[i]);
-    //         }
-    //     }
+        for(int i = k; i < n; i++){
+            if(pq.peek() < nums[i]){
+                // pop the top element and add the current element to it
+                pq.poll();
+                pq.add(nums[i]);
+            }
+        }
 
-    //     // after this operation the peek element would be pointing towards the kth largest element
-    //     return pq.peek();
-    // }
+        // after this operation the peek element would be pointing towards the kth largest element
+        return pq.peek();
+    }
 }
