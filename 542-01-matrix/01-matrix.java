22d05f6082b6ddf21cb9e9a00eb23a1fe6caa1ab
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-05-04 23:55:56 +0530

    Time: 13 ms (84.28%) | Memory: 48.4 MB (9.62%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,10 +1,12 @@
 class Solution {
     private class Pair {
-        int parentNode;
+        int row;
+        int column;
         int distance;
 
-        public Pair(int parentNode, int distance) {
-            this.parentNode = parentNode;
+        public Pair(int row, int column, int distance) {
+            this.row = row;
+            this.column = column;
             this.distance = distance;
         }
     }
@@ -14,19 +16,45 @@ class Solution {
         int n = mat[0].length;
         int v = m * n;
 
-        ArrayList<ArrayList<Integer>> adj = getAdjacencyList(mat);
-
         int[][] distanceMat = new int[m][n];
+        boolean[][] isVisited = new boolean[m][n];
+        Queue<Pair> q = new LinkedList<>();
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (mat[i][j] == 0) {
                     distanceMat[i][j] = 0;
+                    isVisited[i][j] = true;
+                    q.add(new Pair(i, j, distanceMat[i][j]));
+
                 } else {
                     // do the bfs traversal to find the nearest 0;
-                    int distance = findDistanceBybfs(i, j, adj, mat);
+                    // int distance = findDistanceBybfs(i, j, adj, mat);
                     // After rotten oranges, this is the second question, this is because in rotten oranges also we had to traverse level wise, in this also we have to traverse level wise but it is not needed because of the increase in time complexity
-                    distanceMat[i][j] = distance;
+                    isVisited[i][j] = false;
+                }
+            }
+        }
+
+
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+
+        while(!q.isEmpty()){
+            Pair curPair = q.poll();
+
+            int curRow = curPair.row;
+            int curCol = curPair.column;
+            int distance = curPair.distance;
+
+            for(int k = 0; k < 4; k++){
+                int nextRow = dr[k] + curRow;
+                int nextCol = dc[k] + curCol;
+
+                if(nextRow >= 0 && nextRow < m && nextCol >= 0 && nextCol < n && !isVisited[nextRow][nextCol]){
+                    isVisited[nextRow][nextCol] = true;
+                    distanceMat[nextRow][nextCol] = distance + 1;
+                    q.offer(new Pair(nextRow, nextCol, distance + 1)); 
                 }
             }
         }
@@ -34,70 +62,107 @@ class Solution {
         return distanceMat;
     }
 
-    public int findDistanceBybfs(int startRow, int startCol, ArrayList<ArrayList<Integer>> adj, int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
-        int startNode = startRow * n + startCol;
+    // TC: O(m x n)(m x n)
+    // private class Pair {
+    //     int parentNode;
+    //     int distance;
 
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(startNode, 0));
+    //     public Pair(int parentNode, int distance) {
+    //         this.parentNode = parentNode;
+    //         this.distance = distance;
+    //     }
+    // }
 
-        boolean[] visited = new boolean[m * n];
-        visited[startNode] = true;
+    // public int[][] updateMatrix(int[][] mat) {
+    //     int m = mat.length;
+    //     int n = mat[0].length;
+    //     int v = m * n;
 
-        while (!q.isEmpty()) {
-            Pair curPair = q.poll();
-            int currentNode = curPair.parentNode;
-            int distance = curPair.distance;
+    //     ArrayList<ArrayList<Integer>> adj = getAdjacencyList(mat);
 
-            int row = currentNode / n;
-            int col = currentNode % n;
+    //     int[][] distanceMat = new int[m][n];
 
-            if (mat[row][col] == 0) {
-                return distance;
-            }
+    //     for (int i = 0; i < m; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             if (mat[i][j] == 0) {
+    //                 distanceMat[i][j] = 0;
+    //             } else {
+    //                 // do the bfs traversal to find the nearest 0;
+    //                 int distance = findDistanceBybfs(i, j, adj, mat);
+    //                 // After rotten oranges, this is the second question, this is because in rotten oranges also we had to traverse level wise, in this also we have to traverse level wise but it is not needed because of the increase in time complexity
+    //                 distanceMat[i][j] = distance;
+    //             }
+    //         }
+    //     }
 
-            for (int neighborNode : adj.get(currentNode)) {
-                if (!visited[neighborNode]) {
-                    visited[neighborNode] = true;
-                    q.add(new Pair(neighborNode, distance + 1));
-                }
-            }
-        }
+    //     return distanceMat;
+    // }
 
-        // This should ideally not be reached in a valid grid with at least one 0
-        return Integer.MAX_VALUE;
-    }
+    // public int findDistanceBybfs(int startRow, int startCol, ArrayList<ArrayList<Integer>> adj, int[][] mat) {
+    //     int m = mat.length;
+    //     int n = mat[0].length;
+    //     int startNode = startRow * n + startCol;
 
-    public ArrayList<ArrayList<Integer>> getAdjacencyList(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
-        int v = m * n;
+    //     Queue<Pair> q = new LinkedList<>();
+    //     q.add(new Pair(startNode, 0));
 
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>(v);
-        for (int i = 0; i < v; i++) {
-            adj.add(new ArrayList<>());
-        }
+    //     boolean[] visited = new boolean[m * n];
+    //     visited[startNode] = true;
 
-        int[] dr = { -1, 1, 0, 0 };
-        int[] dc = { 0, 0, -1, 1 };
+    //     while (!q.isEmpty()) {
+    //         Pair curPair = q.poll();
+    //         int curNode = curPair.parentNode;
+    //         int distance = curPair.distance;
 
-        for (int row = 0; row < m; row++) {
-            for (int col = 0; col < n; col++) {
-                int curNode = row * n + col;
+    //         // converting 1D into 2d
+    //         int row = curNode / n;
+    //         int col = curNode % n;
 
-                for (int k = 0; k < 4; k++) {
-                    int newRow = dr[k] + row;
-                    int newCol = dc[k] + col;
+    //         if(mat[row][col] == 0){
+    //             return distance;
+    //         }
 
-                    if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n) {
-                        int neighbourNode = newRow * n + newCol;
-                        adj.get(curNode).add(neighbourNode);
-                    }
-                }
-            }
-        }
+    //         for(int adjNode: adj.get(curNode)){
+    //             if(!visited[adjNode]){
+    //                 visited[adjNode] = true;
+    //                 q.add(new Pair(adjNode, distance + 1));
+    //             }
+    //         }
+    //     }
 
-        return adj;
-    }
+    //     // This should ideally not be reached in a valid grid with at least one 0
+    //     return Integer.MAX_VALUE;
+    // }
+
+    // public ArrayList<ArrayList<Integer>> getAdjacencyList(int[][] mat) {
+    //     int m = mat.length;
+    //     int n = mat[0].length;
+    //     int v = m * n;
+
+    //     ArrayList<ArrayList<Integer>> adj = new ArrayList<>(v);
+    //     for (int i = 0; i < v; i++) {
+    //         adj.add(new ArrayList<>());
+    //     }
+
+    //     int[] dr = { -1, 1, 0, 0 };
+    //     int[] dc = { 0, 0, -1, 1 };
+
+    //     for (int row = 0; row < m; row++) {
+    //         for (int col = 0; col < n; col++) {
+    //             int curNode = row * n + col;
+
+    //             for (int k = 0; k < 4; k++) {
+    //                 int newRow = dr[k] + row;
+    //                 int newCol = dc[k] + col;
+
+    //                 if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n) {
+    //                     int neighbourNode = newRow * n + newCol;
+    //                     adj.get(curNode).add(neighbourNode);
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    //     return adj;
+    // }
 }
