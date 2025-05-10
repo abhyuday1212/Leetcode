70e8a39c2c446a75b2dc86bae07fba2f95537860
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-05-10 17:48:24 +0530

    Time: 18 ms (5.93%) | Memory: 45.7 MB (29.48%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.java
@@ -0,0 +1,69 @@
+class Solution {
+    public void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+
+        boolean[][] isVisited = new boolean[m][n];
+
+        // mark the visited wherever '0' are there in the boundary for rows and cols
+
+        // Rows
+        for (int i = 0; i < n; i++) {
+            // first row
+            if (board[0][i] == 'O' && !isVisited[0][i]) {
+                // dfs and mark it as visited
+                dfsTraversal(0, i, isVisited, board);
+            }
+
+            // last row
+            // m is total rows, so last row will be m - 1
+            if (board[m - 1][i] == 'O' && !isVisited[m - 1][i]) {
+                // dfs and mark it as visited
+                dfsTraversal(m - 1, i, isVisited, board);
+            }
+        }
+
+        // Cols
+        for (int j = 0; j < m; j++) {
+            //first col
+            if (board[j][0] == 'O' && !isVisited[j][0]) {
+                dfsTraversal(j, 0, isVisited, board);
+            }
+
+            // last col
+            if (board[j][n - 1] == 'O' && !isVisited[j][n - 1]) {
+                dfsTraversal(j, n - 1, isVisited, board);
+            }
+        }
+
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (board[row][col] == 'O' && !isVisited[row][col]) {
+                    // System.out.println("Did DFS for " + row + " col: " + col);
+                    board[row][col] = 'X';
+                }
+            }
+        }
+    }
+
+    public void dfsTraversal(int curRow, int curCol, boolean[][] isVisited, char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        isVisited[curRow][curCol] = true;
+        System.out.println("Marked the row " + curRow + " and the col " + curCol + " as visited.");
+
+        int[] dr = { -1, 1, 0, 0 };
+        int[] dc = { 0, 0, -1, 1 };
+
+        for (int k = 0; k < 4; k++) {
+            int nextRow = dr[k] + curRow;
+            int nextCol = dc[k] + curCol;
+
+            if (nextRow >= 0 && nextRow < m && nextCol >= 0 && nextCol < n && board[nextRow][nextCol] == 'O'
+                    && !isVisited[nextRow][nextCol]) {
+                dfsTraversal(nextRow, nextCol, isVisited, board);
+            }
+        }
+
+    }
+}
