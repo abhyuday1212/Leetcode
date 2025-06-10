fac64d0534214635118f4c2085a8a3d901445d0d
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-06-10 22:15:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (64.86%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,10 +1,22 @@
 class Solution {
+    // S-0:
+    private Map<Integer, Integer> dp = new HashMap<>(); // you can also create a helper function and pass this into that
+
     public int fib(int n) {
+        // step 3
         if(n<2){
             return n;
         }
 
+        //S-2:
+        if (dp.containsKey(n)) {
+            return dp.get(n);
+        }
+
         //Linear recursive Relation
-        return fib(n-1)+ fib(n-2);
+        // S-1:
+        int result = fib(n - 1) + fib(n - 2);
+        dp.put(n, result);
+        return result;
     }
 }
