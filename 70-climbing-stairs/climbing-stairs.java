48b2cc188cb89551c18602aa223da98f5e59f407
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-06-10 23:29:07 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (65.62%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -2,22 +2,21 @@ class Solution {
     HashMap<Integer, Integer> mp = new HashMap<>();
 
     public int climbStairs(int n) {
-        if(n == 0) return 1;
-        if(n == 1) return 1;
+        if (n == 0)
+            return 1; //n == 0 → return 1 means “we’ve found one valid way that ends exactly at the top (no more steps needed).
+
+        if (n == 1)
+            return 1; // n == 1 → return 1 means “from one step below, there’s exactly one move (a single-step) to finish.”
 
         // memorize it
-        if(mp.containsKey(n)){
+        if (mp.containsKey(n)) {
             return mp.get(n);
         }
 
-        // 1 steps
-        int left = climbStairs(n - 1);
-        mp.put(n-1, left);
+        // 1 steps + 2 steps
+        int ways = climbStairs(n - 1) + climbStairs(n - 2);
+        mp.put(n, ways);
 
-        // 2 steps
-        int right = climbStairs(n - 2);
-        mp.put(n-2, right);
-    
-        return left + right;
+        return ways;
     }
 }
