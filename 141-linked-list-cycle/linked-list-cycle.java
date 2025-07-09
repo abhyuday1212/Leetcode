2277bf4dbba6786d5abc021e7bd20d3d0dda4223
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-07-09 20:16:38 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (76.59%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -1,56 +1,29 @@
 /**
  * Definition for singly-linked list.
  * class ListNode {
- * int val;
- * ListNode next;
- * ListNode(int x) {
- * val = x;
- * next = null;
- * }
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
  * }
  */
 public class Solution {
-    // Optimal: tartosie hare
     public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null)
-            return false;
+        if(head == null) return false;
 
         ListNode slow = head;
         ListNode fast = head;
 
-        while (fast != null && fast.next != null) {
+        while(slow.next != null && fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
-
-            // check if that node already exist
-            if (slow == fast) {
+            if(slow == fast){
                 return true;
             }
         }
 
         return false;
     }
-
-    // Brute: Use a hashMap to check weather that node exist or not
-    // public boolean hasCycle(ListNode head) {
-    // if (head == null || head.next == null)
-    // return false;
-
-    // HashMap<ListNode, Integer> mp = new HashMap<>();
-    // ListNode temp = head;
-
-    // while (temp != null) {
-    // // check if that node already exist
-    // if (mp.containsKey(temp)) {
-    // return true;
-    // }
-
-    // // save
-    // mp.put(temp, mp.getOrDefault(temp, 0) + 1);
-
-    // temp = temp.next;
-    // }
-
-    // return false;
-    // }
 }
