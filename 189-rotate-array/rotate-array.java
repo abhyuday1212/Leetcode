f2a3156aedfa845c98c0df5d1063f01ab0922a8a
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-07-13 12:57:18 +0530

    Time: 19 ms (5.31%) | Memory: 55.4 MB (98.14%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,42 +1,26 @@
 class Solution {
-     public void rotate(int[] nums, int k) {
-
-        // make a copy of nums array
+    public void rotate(int[] nums, int k) {
         List<Integer> temp = new ArrayList<>();
-
-        if (nums.length == 0) {
-            return;
-        }
-
-        if (k == nums.length) {
-            return; // that array will not be rotated
-        } else if (k > nums.length) {
-            k = k % nums.length;
-            if(k == nums.length){
-                return;
-            }
-        }
-
         int n = nums.length;
 
-        // Step: 1 -> Copy the first k elements in a temporary array
-        // {5,6,7}
-        for (int i = n-k; i < n; i++) {
+        if(k > n) k = k % n;
+
+        if(k == 0 || k == n) return;
+
+        int target = n - k - 1;
+
+        for(int i = target + 1; i < n; i++){
             temp.add(nums[i]);
         }
 
-        // Step: 2 -> rotate towards right/ Clockwise
-        //
-        for (int i = 1; i <= n-k; i++) {
-            nums[n - i] = nums[(n - k) - i];
-            // last index(6) = 7-4(4)
-            //
+        for(int i = 0; i <= target; i++){
+            temp.add(nums[i]);
         }
 
-        // copy the remaining nums array fill
-        for (int i = 0; i < k; i++) {
+        System.out.println("List: " + temp);
+
+        for(int i = 0; i < n; i++){
             nums[i] = temp.get(i);
         }
-
     }
 }
