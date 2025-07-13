d3cfd333359181174c28ac6be4670475a952b0d5
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-07-13 13:38:28 +0530

    Time: 1 ms (46.93%) | Memory: 57.4 MB (51.78%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,24 +1,24 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        List<Integer> temp = new ArrayList<>();
         int n = nums.length;
+        int[] temp = new int[n];
 
         if(k > n) k = k % n;
 
         if(k == 0 || k == n) return;
 
-        int target = n - k - 1;
+        int target = n - k;
 
-        for(int i = target + 1; i < n; i++){
-            temp.add(nums[i]);
+        for(int i = target; i < n; i++){
+            temp[i- target] = nums[i];
         }
 
-        for(int i = 0; i <= target; i++){
-            temp.add(nums[i]);
+        for(int i = 0; i < target; i++){
+           temp[k + i] = nums[i];
         }
 
         for(int i = 0; i < n; i++){
-            nums[i] = temp.get(i);
+            nums[i] = temp[i];
         }
     }
 }
