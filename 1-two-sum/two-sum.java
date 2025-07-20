9c3d48fcdf50950920c8bf84509953ea2a146b20
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-07-20 14:55:24 +0530

    Time: 3 ms (61.60%) | Memory: 45.1 MB (30.48%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,28 +1,19 @@
 class Solution {
-    // Brute: Simple For Loop and then adding the element
-    // Better: Hashing (Optimal way if we need to return the index also)
-    
-    // Optimal: 2 Pointer Approach 
-    // Only optimal for Varity 1 problems where we need to return the boolean values as we need to sort the array, and if we sort the array then indexes will get disturb with the end pointer at last and then checking if sum is greater than or less than the current two sum elements
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        HashMap<Integer, Integer> mp = new HashMap<>(); 
+        // hashmap to store complements
 
-    static int[] twoSum(int[] nums, int target) {
-    for (int i = 0; i <= nums.length; i++) {
-    int firstElement = nums[i];
-    for (int j = i + 1; j <= nums.length - 1; j++) {
-    int seccondElem = nums[j];
-    int sum = seccondElem + firstElement;
-    if (sum == target) {
-    // finalAns.add(i);
-    // finalAns.add(j);
-    // return finalAns;
+        for(int i = 0; i < n; i++){
+            int complement = target - nums[i];
 
-    return new int[] { i, j };
-    }
+            if(mp.containsKey(complement)){
+                return new int[]{mp.get(complement), i};
+            }
 
-    }
-    }
+            mp.put(nums[i], i);
+        }
 
-    return new int[0];
-
+        return new int[0];
     }
 }
