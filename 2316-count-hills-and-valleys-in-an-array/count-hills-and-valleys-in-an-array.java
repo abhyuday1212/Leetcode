29212ff9f9256a18428c68482e1bfad645cf0381
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-07-27 12:03:27 +0530

    Time: 1 ms (33.40%) | Memory: 41.7 MB (41.19%) - LeetSync

---
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
@@ -0,0 +1,68 @@
+class Solution {
+    public int countHillValley(int[] nums) {
+        int n = nums.length;
+
+        int[] leftElems = new int[n];
+        int[] rightElems = new int[n];
+
+        for (int i = 1; i < n - 1; i++) {
+            leftElems[i] = findLeftNonEqualElem(i, nums);
+            rightElems[i] = findRightNonEqualElem(i, nums);
+        }
+
+        // System.out.println("Left Elems: " + Arrays.toString(leftElems) );
+        // System.out.println("rightElems: " + Arrays.toString(rightElems));
+
+        int hill = 0;
+        int valley = 0;
+
+        int leftLast = -1;
+        int rightLast = -1;
+
+        for (int i = 1; i < n - 1; i++) {
+            if (leftLast == leftElems[i] && rightLast == rightElems[i]){
+                // System.out.println("1. Reverted back for i: " + i);
+                continue;
+            }
+
+            if(leftElems[i] == 0 || rightElems[i] == 0){
+                // System.out.println("2. Reverted back for i: " + i);
+                continue;
+            }
+
+            // System.out.println("Nums: " + nums[i]);
+
+            if (leftElems[i] < nums[i] && rightElems[i] < nums[i] ) {
+                // System.out.println("Counted hill for index: " + i + " & " + nums[i]);
+                hill++;
+            } else if (nums[i] < leftElems[i] && nums[i] < rightElems[i]) {
+                // System.out.println("Counted valley for index: " + i + " & " + nums[i]);
+                valley++;
+            }
+            leftLast = leftElems[i];
+            rightLast = rightElems[i];
+        }
+
+        return hill + valley;
+    }
+
+    public int findRightNonEqualElem(int curIndex, int[] arr) {
+        for (int i = curIndex + 1; i < arr.length; i++) {
+            if (arr[curIndex] != arr[i]) {
+                return arr[i];
+            }
+        }
+
+        return 0;
+    }
+
+    public int findLeftNonEqualElem(int curIndex, int[] arr) {
+        for (int i = curIndex - 1; i >= 0; i--) {
+            if (arr[curIndex] != arr[i]) {
+                return arr[i];
+            }
+        }
+
+        return 0;
+    }
+}
