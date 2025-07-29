120a4e65385da344f0d40fe84996a5a459ef6fd1
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-07-30 00:51:35 +0530

    Time: 1 ms (71.02%) | Memory: 42.2 MB (63.03%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,39 +1,40 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
-
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        helperFn(root, 0, list);
-        return list;
+        List<Integer> ans = new ArrayList<>();
+        Map<Integer, Integer> mp = new HashMap<>();
+        // index
+
+        dfsHelper(root, mp, ans, 0);
+
+        return ans;
     }
 
-    public void helperFn(TreeNode root, int level, List<Integer> ds) {
+    public void dfsHelper(TreeNode root, Map<Integer, Integer> mp, List<Integer> ans, int level) {
         if (root == null) {
             return;
         }
 
-        // check for elements to add in ds if and only if it is obtained for the first
-        // time in that level
-        if (level == ds.size()) {
-            ds.add(root.val);
+        if (!mp.containsKey(level)) {
+            mp.put(level, root.val);
+            ans.add(root.val);
         }
 
-        // reversed preorder
-        helperFn(root.right, level + 1, ds);
-        helperFn(root.left, level + 1, ds);
+        dfsHelper(root.right, mp, ans, level + 1);
+        dfsHelper(root.left, mp, ans, level + 1);
     }
 }
