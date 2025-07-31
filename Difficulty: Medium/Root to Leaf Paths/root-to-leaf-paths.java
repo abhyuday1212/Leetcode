3a5977a55e54476ac074d90816ca12718b4d0c14
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-07-31 21:09:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Root to Leaf Paths/root-to-leaf-paths.java
+++ Difficulty: Medium/Root to Leaf Paths/root-to-leaf-paths.java
@@ -1,136 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Node {
-    int data;
-    Node left;
-    Node right;
-
-    Node(int data) {
-        this.data = data;
-        left = null;
-        right = null;
-    }
-
-    public static Node buildTree(String str) {
-        // Corner Case
-        if (str.length() == 0 || str.charAt(0) == 'N') return null;
-
-        // Creating array of Strings from input
-        // String after spliting by space
-        String ip[] = str.split(" ");
-
-        // Create the root of the tree
-        Node root = new Node(Integer.parseInt(ip[0]));
-
-        // Push the root to the queue
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
-
-        // Starting from the second element
-        int i = 1;
-        while (queue.size() > 0 && i < ip.length) {
-
-            // Get and remove the front of the queue
-            Node currNode = queue.peek();
-            queue.remove();
-
-            // Get the current node's value from the string
-            String currVal = ip[i];
-
-            // If the left child is not null
-            if (!currVal.equals("N")) {
-
-                // Create the left child for the current node
-                currNode.left = new Node(Integer.parseInt(currVal));
-                // Push it to the queue
-                queue.add(currNode.left);
-            }
-
-            // For the right child
-            i++;
-            if (i >= ip.length) break;
-
-            currVal = ip[i];
-
-            // If the right child is not null
-            if (!currVal.equals("N")) {
-
-                // Create the right child for the current node
-                currNode.right = new Node(Integer.parseInt(currVal));
-
-                // Push it to the queue
-                queue.add(currNode.right);
-            }
-            i++;
-        }
-
-        return root;
-    }
-
-    public static Node inputTree(BufferedReader br) throws IOException {
-        return buildTree(br.readLine().trim());
-    }
-
-    public static void inorder(Node root) {
-        if (root == null) return;
-        inorder(root.left);
-        System.out.print(root.data + " ");
-        inorder(root.right);
-    }
-}
-
-class IntMatrix {
-    public static int[][] input(BufferedReader br, int n, int m) throws IOException {
-        int[][] mat = new int[n][];
-
-        for (int i = 0; i < n; i++) {
-            String[] s = br.readLine().trim().split(" ");
-            mat[i] = new int[s.length];
-            for (int j = 0; j < s.length; j++) mat[i][j] = Integer.parseInt(s[j]);
-        }
-
-        return mat;
-    }
-
-    public static void print(int[][] m) {
-        for (var a : m) {
-            for (int e : a) System.out.print(e + " ");
-            System.out.println();
-        }
-    }
-
-    public static void print(ArrayList<ArrayList<Integer>> m) {
-        for (var a : m) {
-            for (int e : a) System.out.print(e + " ");
-            System.out.println();
-        }
-    }
-}
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t;
-        t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-
-            Node root = Node.inputTree(br);
-
-            Solution obj = new Solution();
-            ArrayList<ArrayList<Integer>> res = obj.Paths(root);
-
-            IntMatrix.print(res);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 /*
 
 Definition for Binary Tree Node
@@ -149,30 +16,60 @@ class Node
 }
 */
 
-class Solution {
+class Solution{
     public static ArrayList<ArrayList<Integer>> Paths(Node root) {
-       return pathsHelper(root, new ArrayList<>());
+        return recursiveHelper(root, new ArrayList<>());
     }
     
-    public static ArrayList<ArrayList<Integer>> pathsHelper(Node root, ArrayList<Integer> ds) {
+    public static ArrayList<ArrayList<Integer>> recursiveHelper(Node root, ArrayList<Integer> ds){
         ArrayList<ArrayList<Integer>> baseAns = new ArrayList<>();
-
+        
         if (root == null) {
             return baseAns;
         }
-
-        //self work
+        
         ds.add(root.data);
-
-        if (root.left == null && root.right == null) {
+        
+        if(root.left == null && root.right == null){
             baseAns.add(new ArrayList<>(ds));
-        } else {
-            baseAns.addAll(pathsHelper(root.left, ds));
-            baseAns.addAll(pathsHelper(root.right, ds));
         }
-
+        else{
+            baseAns.addAll(recursiveHelper(root.left, ds));
+            baseAns.addAll(recursiveHelper(root.right, ds));
+        }
+        
         ds.remove(ds.size() - 1);
-
         return baseAns;
     }
 }
+
+
+// class Solution {
+//     public static ArrayList<ArrayList<Integer>> Paths(Node root) {
+//         ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+//         ArrayList<Integer> ds = new ArrayList<>();
+        
+//         getAllPath(root, ans, ds);
+        
+//         return ans;
+//     }
+    
+//     public static void getAllPath(Node root, ArrayList<ArrayList<Integer>> ans, ArrayList<Integer> ds){
+//         if(root.left == null && root.right == null){
+//             ds.add(root.data);
+//             ans.add(new ArrayList<>(ds));
+//             // System.out.println("Ans: " + ans);
+//             // ds.remove(ds.size() - 1);
+//             return;
+//         }
+        
+//         ds.add(root.data);
+        
+//         getAllPath(root.left, ans, ds);
+//         ds.remove(ds.size() - 1);
+        
+//         getAllPath(root.right, ans, ds);
+//         ds.remove(ds.size() - 1);
+//     }
+// }
+
