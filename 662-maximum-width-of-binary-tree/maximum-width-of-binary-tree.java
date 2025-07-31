b18261df9e5cebbecec3bc7a02bda52b2a763282
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-08-01 01:09:39 +0530

    Time: 2 ms (37.22%) | Memory: 44.4 MB (5.35%) - LeetSync

---
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<Integer, Integer> firstDepthIndex = new HashMap<>();
+    // depth, index
+
+    int maxWidth = 0;
+
+    public int widthOfBinaryTree(TreeNode root) {
+        dfs(root, 0, 1);
+        // root, dept, index
+        return maxWidth;
+    }
+
+    public void dfs(TreeNode root, int depth, long index){
+        if(root == null){
+            return;
+        }
+
+        // if at this depth, map doesnt contains the index then only store the depth at this level
+        if(!firstDepthIndex.containsKey(depth)){
+            firstDepthIndex.put(depth, (int)index);
+        }
+
+        int firstIndex = firstDepthIndex.get(depth);
+        int curWidth = (int)index - firstIndex + 1;
+        maxWidth = Math.max(maxWidth, curWidth);
+
+        dfs(root.left, depth + 1, 2*index);
+        dfs(root.right, depth + 1, 2*index + 1);
+    }
+}
