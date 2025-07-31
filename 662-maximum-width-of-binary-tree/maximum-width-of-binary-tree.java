61680543676a55fac269f55fd63b1cddc6871041
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-08-01 01:15:39 +0530

    Time: 1 ms (98.93%) | Memory: 44.5 MB (5.35%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -13,6 +13,12 @@
  *     }
  * }
  */
+
+/*
+Zero Based - Indexing
+1st Elem = 2 * i + 1
+2nd Elem = 2 * i + 2
+ */
 class Solution {
     Map<Integer, Integer> firstDepthIndex = new HashMap<>();
     // depth, index
@@ -20,26 +26,62 @@ class Solution {
     int maxWidth = 0;
 
     public int widthOfBinaryTree(TreeNode root) {
-        dfs(root, 0, 1);
+        dfs(root, 0, 0);
         // root, dept, index
         return maxWidth;
     }
 
-    public void dfs(TreeNode root, int depth, long index){
-        if(root == null){
+    public void dfs(TreeNode root, int depth, long index) {
+        if (root == null) {
             return;
         }
 
         // if at this depth, map doesnt contains the index then only store the depth at this level
-        if(!firstDepthIndex.containsKey(depth)){
-            firstDepthIndex.put(depth, (int)index);
+        if (!firstDepthIndex.containsKey(depth)) {
+            firstDepthIndex.put(depth, (int) index);
         }
 
         int firstIndex = firstDepthIndex.get(depth);
-        int curWidth = (int)index - firstIndex + 1;
+        int curWidth = (int) index - firstIndex + 1;
         maxWidth = Math.max(maxWidth, curWidth);
 
-        dfs(root.left, depth + 1, 2*index);
-        dfs(root.right, depth + 1, 2*index + 1);
+        dfs(root.left, depth + 1, 2 * index + 1);
+        dfs(root.right, depth + 1, 2 * index + 2);
     }
 }
+
+/*
+1-based indexing
+1st Elem = 2 * i
+2nd Elem = 2 * i + 1
+ */
+// class Solution {
+//     Map<Integer, Integer> firstDepthIndex = new HashMap<>();
+//     // depth, index
+
+//     int maxWidth = 0;
+
+//     public int widthOfBinaryTree(TreeNode root) {
+//         dfs(root, 0, 1);
+//         // root, dept, index
+//         return maxWidth;
+//     }
+
+//     public void dfs(TreeNode root, int depth, long index){
+//         if(root == null){
+//             return;
+//         }
+
+//         // if at this depth, map doesnt contains the index then only store the depth at this level
+//         if(!firstDepthIndex.containsKey(depth)){
+//             firstDepthIndex.put(depth, (int)index);
+//         }
+
+//         int firstIndex = firstDepthIndex.get(depth);
+//         int curWidth = (int)index - firstIndex + 1;
+//         maxWidth = Math.max(maxWidth, curWidth);
+
+//         dfs(root.left, depth + 1, 2*index);
+//         dfs(root.right, depth + 1, 2*index + 1);
+//     }
+// }
