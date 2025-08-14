0fee1530202bc3f46a4b48c45f6d5899042191de
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-08-15 01:50:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (71.12%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
@@ -1,24 +1,36 @@
 class Solution {
+    // 0ms
     public String largestGoodInteger(String num) {
-        int n = num.length();
-        char maxChar = '\0'; // null character
-
-        for (int i = 2; i < n; i++) {
-            char curChar = num.charAt(i);
-            char prevChar = num.charAt(i - 1);
-            char prevPrevChar = num.charAt(i - 2);
-
-            if (curChar == prevChar && prevChar == prevPrevChar) {
-                if (curChar > maxChar) {
-                    maxChar = curChar;
-                }
+        String[] numbers = { "999", "888", "777", "666", "555", "444", "333", "222", "111", "000" };
+        for (String i : numbers) {
+            if (num.contains(i)) {
+                return i;
             }
         }
-
-        if (maxChar == '\0') {
-            return "";
-        }
-
-        return "" + maxChar + maxChar + maxChar;
+        return "";
     }
+
+    // 7ms
+    // public String largestGoodInteger(String num) {
+    //     int n = num.length();
+    //     char maxChar = '\0'; // null character
+
+    //     for (int i = 2; i < n; i++) {
+    //         char curChar = num.charAt(i);
+    //         char prevChar = num.charAt(i - 1);
+    //         char prevPrevChar = num.charAt(i - 2);
+
+    //         if (curChar == prevChar && prevChar == prevPrevChar) {
+    //             if (curChar > maxChar) {
+    //                 maxChar = curChar;
+    //             }
+    //         }
+    //     }
+
+    //     if (maxChar == '\0') {
+    //         return "";
+    //     }
+
+    //     return "" + maxChar + maxChar + maxChar;
+    // }
 }
