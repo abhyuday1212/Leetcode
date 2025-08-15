b9742d3b084791eccf2901e6e9fb1c1dc9368cbf
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-08-15 18:19:18 +0530

    Time: 2 ms (86.50%) | Memory: 41.8 MB (93.21%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -2,13 +2,20 @@ class Solution {
     // Hume check karna hai ki kya s ko aise tod sakte hain (segment kar sakte hain) ki har segment wordDict me ho.
     // The intuition is that you will have consider every combination of words in the wordDict, moreover these words can be used more than once when computing the combinations.
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> dict = new HashSet<>(wordDict);
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
+        // Set<String> dict = new HashSet<>(wordDict);
 
         for (int i = 1; i <= s.length(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && dict.contains(s.substring(j, i))) {
+            // for (int j = 0; j < i; j++) {
+            //     if (dp[j] && dict.contains(s.substring(j, i))) {
+            //         dp[i] = true;
+            //         break;
+            //     }
+            // }
+             for (String w : wordDict) {
+                int start = i - w.length();
+                if (start >= 0 && dp[start] && s.substring(start, i).equals(w)) {
                     dp[i] = true;
                     break;
                 }
