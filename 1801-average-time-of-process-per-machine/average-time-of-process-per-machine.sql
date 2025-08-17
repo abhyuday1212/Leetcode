c057ca3083900474c358ea691f615952c63eb656
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-08-17 13:16:34 +0530

    Time: 243 ms (61.15%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -2,9 +2,9 @@
 select t1.machine_id, round(avg(t2.timestamp - t1.timestamp),3) as processing_time
 from Activity as t1
 join Activity as t2
-    on t1.machine_id = t2.machine_id
-   and t1.process_id = t2.process_id
-   and t1.activity_type = 'start'
-   and t2.activity_type = 'end'
+    on t1.machine_id = t2.machine_id   -- both rows belong to same machine
+    and t1.process_id = t2.process_id   -- both rows are for the same process
+    and t1.activity_type = 'start'      -- left side must be a start
+    and t2.activity_type = 'end'        -- right side must be an end
 group by machine_id
 order by machine_id asc
