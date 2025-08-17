13cc56fc7c5b96c1303cc2e6a0304f31b7f6ccca
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-08-17 23:42:21 +0530

    Time: 6 ms (98.66%) | Memory: 47.6 MB (91.58%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,13 +1,12 @@
 class Solution {
-    // BRUT: O(N⋅KlogK).
     public List<List<String>> groupAnagrams(String[] strs) {
         if (strs == null || strs.length == 0) {
             return new ArrayList<>();
         }
 
         int n = strs.length;
-        List<String> sortedStrs = new ArrayList<>();
-        HashMap<String, Integer> mp = new HashMap<>();
+
+        HashMap<String, List<String>> mp = new HashMap<>();
 
         List<List<String>> ans = new ArrayList<>();
 
@@ -15,21 +14,48 @@ class Solution {
             String cur = strs[i]; // list
             char[] chArr = cur.toCharArray();
             Arrays.sort(chArr);
-            
+
             String sortedStr = new String(chArr);
-
-            if (!mp.containsKey(sortedStr)) {
-                mp.put(sortedStr, ans.size());
-                ans.add(new ArrayList<>());
-            }
-
-            int index = mp.get(sortedStr);
-
-            // get the inner list from the ans
-            ans.get(index).add(strs[i]);
-
+ 
+            mp.computeIfAbsent(sortedStr, k -> new ArrayList<>()).add(cur);
+             
         }
+
+        ans.addAll(mp.values());
 
         return ans;
     }
+    // BRUT: O(N⋅KlogK).
+    // public List<List<String>> groupAnagrams(String[] strs) {
+    //     if (strs == null || strs.length == 0) {
+    //         return new ArrayList<>();
+    //     }
+
+    //     int n = strs.length;
+    //     List<String> sortedStrs = new ArrayList<>();
+    //     HashMap<String, Integer> mp = new HashMap<>();
+
+    //     List<List<String>> ans = new ArrayList<>();
+
+    //     for (int i = 0; i < n; i++) {
+    //         String cur = strs[i]; // list
+    //         char[] chArr = cur.toCharArray();
+    //         Arrays.sort(chArr);
+
+    //         String sortedStr = new String(chArr);
+
+    //         if (!mp.containsKey(sortedStr)) {
+    //             mp.put(sortedStr, ans.size());
+    //             ans.add(new ArrayList<>());
+    //         }
+
+    //         int index = mp.get(sortedStr);
+
+    //         // get the inner list from the ans
+    //         ans.get(index).add(strs[i]);
+
+    //     }
+
+    //     return ans;
+    // }
 }
