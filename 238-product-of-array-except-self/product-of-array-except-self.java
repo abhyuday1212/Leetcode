85c45addda746eff4b89d8c0746014051e69f039
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-08-18 03:14:17 +0530

    Time: 3 ms (19.12%) | Memory: 55.4 MB (61.68%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,33 +1,50 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] prefixMultiplyArr1 = new int[n];
-        int[] prefixMultiplyArr2 = new int[n];
-
-        int temp1 = 1;
-        for(int i = 0; i < n; i++){
-            temp1 *= nums[i];
-            prefixMultiplyArr1[i] = temp1;
+        int[] output = new int[n];
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            output[i] = prefix;
+            prefix *= nums[i];
         }
-        
-        int temp2 = 1;
-        for(int i = n - 1; i >= 0; i--){
-            temp2 *= nums[i];
-            prefixMultiplyArr2[i] = temp2;
+        int suffixProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            output[i] *= suffixProduct;
+            suffixProduct *= nums[i];
         }
-
-        // System.out.println("prefixMultiplyArr1: " + Arrays.toString(prefixMultiplyArr1));
-        // System.out.println("prefixMultiplyArr2: " + Arrays.toString(prefixMultiplyArr2));
-
-        int[] ans = new int[n];
-        ans[0] = prefixMultiplyArr2[1];
-        ans[n - 1] = prefixMultiplyArr1[n - 2];
-
-        for(int i = 1; i < n - 1; i++){
-            int prod = prefixMultiplyArr1[i - 1] * prefixMultiplyArr2[i + 1];
-            ans[i] = prod;
-        }
-
-        return ans;
+        return output;
     }
+
+    // BRUT: O(3n)
+    // public int[] productExceptSelf(int[] nums) {
+    //     int n = nums.length;
+    //     int[] prefixMultiplyArr1 = new int[n];
+    //     int[] prefixMultiplyArr2 = new int[n];
+
+    //     int temp1 = 1;
+    //     for(int i = 0; i < n; i++){
+    //         temp1 *= nums[i];
+    //         prefixMultiplyArr1[i] = temp1;
+    //     }
+
+    //     int temp2 = 1;
+    //     for(int i = n - 1; i >= 0; i--){
+    //         temp2 *= nums[i];
+    //         prefixMultiplyArr2[i] = temp2;
+    //     }
+
+    //     // System.out.println("prefixMultiplyArr1: " + Arrays.toString(prefixMultiplyArr1));
+    //     // System.out.println("prefixMultiplyArr2: " + Arrays.toString(prefixMultiplyArr2));
+
+    //     int[] ans = new int[n];
+    //     ans[0] = prefixMultiplyArr2[1];
+    //     ans[n - 1] = prefixMultiplyArr1[n - 2];
+
+    //     for(int i = 1; i < n - 1; i++){
+    //         int prod = prefixMultiplyArr1[i - 1] * prefixMultiplyArr2[i + 1];
+    //         ans[i] = prod;
+    //     }
+
+    //     return ans;
+    // }
 }
