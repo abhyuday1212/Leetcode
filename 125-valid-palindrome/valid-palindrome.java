d08303eacd2fc03a0d391b31ed88eee527bc223b
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-08-19 11:46:59 +0530

    Time: 1 ms (100.00%) | Memory: 43.3 MB (61.82%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -4,17 +4,37 @@ class Solution {
             return true;
         }
 
-        String modifiedStr = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        // String modifiedStr = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-        int n = modifiedStr.length();
+        int n = s.length();
         int l = 0, r = n - 1;
+        char[] ch = s.toCharArray();
 
-        while(l <= r){
-            if(modifiedStr.charAt(l) != modifiedStr.charAt(r)){
+        while (l <= r) {
+            if (ch[l] >= 'A' && ch[l] <= 'Z') {
+                ch[l] += 32;
+            }
+
+            if (ch[r] >= 'A' && ch[r] <= 'Z') {
+                ch[r] += 32;
+            }
+
+            if (!((ch[l] >= 'a' && ch[l] <= 'z') || (ch[l] >= '0' && ch[l] <= '9'))) {
+                l++;
+                continue;
+            }
+            if (!((ch[r] >= 'a' && ch[r] <= 'z') || (ch[r] >= '0' && ch[r] <= '9'))) {
+                r--;
+                continue;
+            }
+
+            if (ch[l] != ch[r]) {
                 return false;
             }
+
             l++;
             r--;
+
         }
 
         return true;
