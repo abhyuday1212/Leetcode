985b817bb63681d3f15fca382bc803fb4a791d81
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-08-26 23:47:33 +0530

    Time: 2 ms (93.69%) | Memory: 56.1 MB (40.83%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -1,30 +1,26 @@
 class Solution {
-    // TC: O(n)
     public boolean lemonadeChange(int[] bills) {
         int five = 0, ten = 0;
         int n = bills.length;
 
         for (int i = 0; i < n; i++) {
             if (bills[i] == 5) {
+                // check 
                 five += 1;
             } else if (bills[i] == 10) {
-                if (five != 0) {
-                    five -= 1;
-                    ten += 1;
-                }
-                else{
+                ten += 1;
+                if (five > 0) {
+                    five = five - 1;
+                } else {
                     return false;
                 }
-            }
-            else{
-                if(ten != 0 && five != 0){
+            } else {
+                if (ten > 0 && five > 0) {
                     five -= 1;
                     ten -= 1;
-                }
-                else if(five >= 3){
+                } else if (five >= 3) {
                     five -= 3;
-                }
-                else{
+                } else {
                     return false;
                 }
             }
