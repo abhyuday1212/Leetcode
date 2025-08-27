edc795e213ab7ee68bad78f916b5915b4fcc6be5
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-08-27 13:09:09 +0530

    Time: 2 ms (83.53%) | Memory: 45.8 MB (56.19%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,26 +1,46 @@
 // BRUT
 class Solution {
     public boolean canJump(int[] nums) {
-        Boolean[] dp = new Boolean[nums.length];
+        int n = nums.length;
+        int maxIndex = 0;
 
-        return helper(nums, 0, dp);
-    }
+        for (int i = 0; i < n; i++) {
+            maxIndex = Math.max(maxIndex, nums[i] + i);
 
-    public boolean helper(int[] nums, int index, Boolean[] dp) {
-        if (index >= nums.length || index == nums.length - 1) {
-            return true;
-        }
-
-          if (dp[index] != null) {
-            return dp[index];
-        }
-
-        for (int i = 1; i <= nums[index]; i++) {
-            if (helper(nums, index + i, dp)) {
-                return dp[index] = true;
+            if(nums[i] == 0 && maxIndex < n - 1 && i >= maxIndex){
+                break;
+            }
+            else if (maxIndex >= n - 1) {
+                // System.out.println("MAxIndex: " + maxIndex);
+                // System.out.println("nums[i]: " + nums[i]);
+                return true;
             }
         }
 
-        return dp[index] = false;
+        return false;
     }
+
+    // public boolean canJump(int[] nums) {
+    //     Boolean[] dp = new Boolean[nums.length];
+
+    //     return helper(nums, 0, dp);
+    // }
+
+    // public boolean helper(int[] nums, int index, Boolean[] dp) {
+    //     if (index >= nums.length || index == nums.length - 1) {
+    //         return true;
+    //     }
+
+    //       if (dp[index] != null) {
+    //         return dp[index];
+    //     }
+
+    //     for (int i = 1; i <= nums[index]; i++) {
+    //         if (helper(nums, index + i, dp)) {
+    //             return dp[index] = true;
+    //         }
+    //     }
+
+    //     return dp[index] = false;
+    // }
 }
