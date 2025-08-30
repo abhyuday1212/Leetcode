3c33e9566830967d6a8092c790dfd410f4a7eef0
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-08-30 16:25:17 +0530

    Time: 8 ms (85.41%) | Memory: 46.6 MB (73.67%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,26 +1,26 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        // similar to insert interval
         int n = intervals.length;
 
-        // sort on the basis of start index
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        List<int[]> ans = new ArrayList<>();
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]); // sort by start
 
-        // Add the first interval.
-        ans.add(new int[] { intervals[0][0], intervals[0][1] });
- 
-        for (int i = 1; i < n; i++) {
-            int[] lastInterval = ans.get(ans.size() - 1);
-            
-            // Check if there is an overlap.
-            if (lastInterval[1] >= intervals[i][0]) {
-                lastInterval[1] = Math.max(lastInterval[1], intervals[i][1]);
-            } else {
-                // no overlap
-                ans.add(new int[] { intervals[i][0], intervals[i][1] });
+        List<int[]> ans = new ArrayList<>();
+        int[] current = intervals[0]; 
+
+        for(int i = 1; i < n; i++){
+            // overlap -> merge
+            if(intervals[i][0] <= current[1]){
+                // pichle ka end bda h current k start se
+                current[1] = Math.max(intervals[i][1], current[1]);
+            }
+            else{
+                // no overlap -> push current and reset
+                ans.add(current);
+                current = intervals[i];
             }
         }
+
+        ans.add(current);
 
         return ans.toArray(new int[ans.size()][]);
     }
