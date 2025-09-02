741142021f63fd6b7abe90f74ae227d9a48e1c25
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-09-03 01:05:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -1,78 +1,26 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-
-// } Driver Code Ends
-
-// User function Template for Java
 class Solution {
     // Function to return Breadth First Search Traversal of given graph.
     public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
-        ArrayList<Integer> bfs = new ArrayList<>();
-        
-        boolean[] visited = new boolean[adj.size()];
+        boolean[] isVisited = new boolean[adj.size()];
+        ArrayList<Integer> bfsList = new ArrayList<>();
         
         Queue<Integer> q = new LinkedList<>();
         
-        q.add(0);
-        visited[0] = true;
+        q.offer(0);
+        isVisited[0] = true;
         
         while(!q.isEmpty()){
-            Integer node = q.poll();
-            bfs.add(node); // printing the current node
+            Integer curNode = q.poll();
+            bfsList.add(curNode);
             
-            // Get all the elements of the adjacenct node of the dequed element
-            // If the adjacency node has been visited the mark it as visited, otherwise,
-            // dont visit that node
-            
-            for(Integer currAdjElem: adj.get(node)){
-                if(visited[currAdjElem] == false){
-                    // add the adjacemt element in the queue
-                    q.add(currAdjElem);
-                    visited[currAdjElem] = true;
+            for(int elems:adj.get(curNode)){
+                if(!isVisited[elems]){
+                    isVisited[elems] = true;
+                    q.offer(elems);
                 }
             }
-            
         }
         
-        return bfs;
+        return bfsList;
     }
 }
-
-
-//{ Driver Code Starts.
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
-            int V = Integer.parseInt(br.readLine().trim());
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
-            for (int i = 0; i < V; i++) {
-                String[] input = br.readLine().trim().split(" ");
-                ArrayList<Integer> node = new ArrayList<>();
-                for (String s : input) {
-                    if (!s.isEmpty()) {
-                        node.add(Integer.parseInt(s));
-                    }
-                }
-                adj.add(node);
-            }
-
-            Solution obj = new Solution();
-            ArrayList<Integer> ans = obj.bfs(adj);
-            for (int num : ans) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
