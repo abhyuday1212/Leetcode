4967ef029cd727fd795c010a65e1ff1f649820d3
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-09-18 23:35:32 +0530

    Time: 18 ms (14.80%) | Memory: 44.8 MB (25.76%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -9,57 +9,57 @@ class Solution {
     // same keys there values are same then we say it is a valid anagram
 
     // Implementing Approach 3
-    // public boolean isAnagram(String s, String t) {
-    //     if (s.length() != t.length())
-    //         return false;
-
-    //     // Create two hash tables;
-    //     HashMap<Character, Integer> mapS = new HashMap<>();
-    //     HashMap<Character, Integer> mapT = new HashMap<>();
-
-    //     // precompute
-    //     for (char key1 : s.toCharArray()) {
-    //         mapS.put(key1, mapS.getOrDefault(key1, 0) + 1);
-    //     }
-
-    //     for (char key2 : t.toCharArray()) {
-    //         mapT.put(key2, mapT.getOrDefault(key2, 0) + 1);
-    //     }
-    //     // System.out.println(mapS);
-    //     // System.out.println(mapT);
-
-    //     boolean ans = true;
-
-    //     for (char key : mapT.keySet()) { // a = 1, c =3
-    //         System.out.println(mapT.get(key));
-    //         System.out.println(mapS.get(key));
-    //         if (!mapT.get(key).equals(mapS.get(key))) {
-    //             System.out.println(mapT.get(key));
-    //             ans = false;
-    //             break;
-    //         }
-    //     }
-
-    //     return ans;
-    // }
-    
-    // Fact: for a Anagram, if we sort the characters then it will be same
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length())
             return false;
 
-        char[] charArr1 = s.toCharArray();
-        char[] charArr2 = t.toCharArray();
+        // Create two hash tables;
+        HashMap<Character, Integer> mapS = new HashMap<>();
+        HashMap<Character, Integer> mapT = new HashMap<>();
 
-        Arrays.sort(charArr1);
-        Arrays.sort(charArr2);
+        // precompute
+        for (char key1 : s.toCharArray()) {
+            mapS.put(key1, mapS.getOrDefault(key1, 0) + 1);
+        }
 
-        for(int i = 0; i < s.length(); i++){
-            if(charArr1[i] != charArr2[i]){
-                return false;
+        for (char key2 : t.toCharArray()) {
+            mapT.put(key2, mapT.getOrDefault(key2, 0) + 1);
+        }
+        // System.out.println(mapS);
+        // System.out.println(mapT);
+
+        boolean ans = true;
+
+        for (char key : mapT.keySet()) { // a = 1, c =3
+            System.out.println(mapT.get(key));
+            System.out.println(mapS.get(key));
+            if (!mapT.get(key).equals(mapS.get(key))) {
+                System.out.println(mapT.get(key));
+                ans = false;
+                break;
             }
         }
 
-        return true;
+        return ans;
     }
+    
+    // Fact: for a Anagram, if we sort the characters then it will be same
+    // public boolean isAnagram(String s, String t) {
+    //     if (s.length() != t.length())
+    //         return false;
+
+    //     char[] charArr1 = s.toCharArray();
+    //     char[] charArr2 = t.toCharArray();
+
+    //     Arrays.sort(charArr1);
+    //     Arrays.sort(charArr2);
+
+    //     for(int i = 0; i < s.length(); i++){
+    //         if(charArr1[i] != charArr2[i]){
+    //             return false;
+    //         }
+    //     }
+
+    //     return true;
+    // }
 }
