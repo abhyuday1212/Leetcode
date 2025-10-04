9d7c3a4b23d66133bc4dad91156741b7b8d7e09f
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-10-04 23:52:06 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (40.59%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,44 +1,36 @@
 class Solution {
     public int trap(int[] height) {
-        int sum = 0;
         int n = height.length;
-        int[] leftMax = prefixMax(height);
-        int[] rightMax = suffixMax(height);
+        int total =0;
+        int leftMax = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (height[i] < leftMax[i] && height[i] < rightMax[i]) {
-                sum = sum + (Math.min(leftMax[i], rightMax[i])) - height[i];
+        int rightMax = 0;
+        int l = 0, r = n - 1;
+
+        while(l < r){
+            if(height[l] <= height[r]){
+                // traverse from left word
+                if(leftMax > height[l]){
+                    total += leftMax - height[l];
+                }
+                else{
+                    // the height at which you are at is leftmax itselt
+                    leftMax = height[l];
+                }
+                l = l + 1;
+            }
+            else{
+                // traverse from right word
+                if(height[r] < rightMax){
+                    total += rightMax - height[r];
+                }
+                else{
+                    rightMax = height[r];
+                }
+                r = r - 1;
             }
         }
 
-        return sum;
-    }
-
-    public int[] prefixMax(int[] height) {
-        // it has similar concept of prefixMax
-        int n = height.length;
-        int[] prefix = new int[n];
-
-        prefix[0] = height[0];
-
-        for (int i = 1; i < n; i++) {
-            prefix[i] = Math.max(height[i], prefix[i - 1]);
-        }
-
-        return prefix;
-    }
-
-    public int[] suffixMax(int[] height) {
-        // it has similar concept of suffixMax
-        int n = height.length;
-        int[] suffix = new int[n];
-
-        suffix[n - 1] = height[n - 1];
-
-        for (int i = n - 2; 0 <= i; i--) {
-            suffix[i] = Math.max(height[i], suffix[i + 1]);
-        }
-
-        return suffix;
+        return total;
     }
 }
