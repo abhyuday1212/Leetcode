b38285e75958a117af532596455d41ed3b8c2b06
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-10-05 20:00:54 +0530

    Time: 24 ms (36.03%) | Memory: 40.5 MB (82.12%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        boolean[] arr = new boolean[n + 1];
+
+        int startIndex = k % n;
+
+        if (startIndex == 0) {
+            startIndex = n;
+        }
+
+        return recursiveHelper(arr, k, startIndex, n);
+    }
+
+    public int recursiveHelper(boolean[] arr, int k, int index, int remaining) {
+        int n = arr.length - 1;
+
+        if (remaining == 1) {
+            for (int i = 1; i <= n; i++) {
+                if (!arr[i]) {
+                    return i;
+                }
+
+            }
+        }
+
+        arr[index] = true;
+
+        int nextIndex = index;
+
+        int cnt = 0;
+
+        while (cnt < k) {
+            nextIndex++;
+
+            if (nextIndex > n) {
+                nextIndex = 1;
+            }
+
+            if (!arr[nextIndex]) {
+                cnt++;
+            }
+        }
+
+        return recursiveHelper(arr, k, nextIndex, remaining - 1);
+    }
+
+}
