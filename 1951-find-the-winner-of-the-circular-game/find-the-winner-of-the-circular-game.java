2981c7d525666a1bf6355ccf046e0062c27e9122
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-10-05 20:19:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (94.80%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,47 +1,62 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        boolean[] arr = new boolean[n + 1];
-
-        int startIndex = k % n;
-
-        if (startIndex == 0) {
-            startIndex = n;
-        }
-
-        return recursiveHelper(arr, k, startIndex, n);
+        return josephus(n, k) + 1;
     }
 
-    public int recursiveHelper(boolean[] arr, int k, int index, int remaining) {
-        int n = arr.length - 1;
-
-        if (remaining == 1) {
-            for (int i = 1; i <= n; i++) {
-                if (!arr[i]) {
-                    return i;
-                }
-
-            }
+    private int josephus(int n, int k) {
+        if (n == 1) {
+            return 0;
         }
 
-        arr[index] = true;
-
-        int nextIndex = index;
-
-        int cnt = 0;
-
-        while (cnt < k) {
-            nextIndex++;
-
-            if (nextIndex > n) {
-                nextIndex = 1;
-            }
-
-            if (!arr[nextIndex]) {
-                cnt++;
-            }
-        }
-
-        return recursiveHelper(arr, k, nextIndex, remaining - 1);
+        return (josephus(n - 1, k) + k) % n;
     }
-
 }
+
+// BRUT
+// class Solution {
+//     public int findTheWinner(int n, int k) {
+//         boolean[] arr = new boolean[n + 1];
+
+//         int startIndex = k % n;
+
+//         if (startIndex == 0) {
+//             startIndex = n;
+//         }
+
+//         return recursiveHelper(arr, k, startIndex, n);
+//     }
+
+//     public int recursiveHelper(boolean[] arr, int k, int index, int remaining) {
+//         int n = arr.length - 1;
+
+//         if (remaining == 1) {
+//             for (int i = 1; i <= n; i++) {
+//                 if (!arr[i]) {
+//                     return i;
+//                 }
+
+//             }
+//         }
+
+//         arr[index] = true;
+
+//         int nextIndex = index;
+
+//         int cnt = 0;
+
+//         while (cnt < k) {
+//             nextIndex++;
+
+//             if (nextIndex > n) {
+//                 nextIndex = 1;
+//             }
+
+//             if (!arr[nextIndex]) {
+//                 cnt++;
+//             }
+//         }
+
+//         return recursiveHelper(arr, k, nextIndex, remaining - 1);
+//     }
+
+// }
