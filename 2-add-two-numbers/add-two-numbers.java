3fb7a8b65bb7ba406af09bf908affac26f7e9e98
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2025-12-06 13:06:47 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (37.68%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -1,4 +1,3 @@
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -9,67 +8,42 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-import java.math.BigInteger;
-
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        List<Long> list1 = new ArrayList<>();
-        List<Long> list2 = new ArrayList<>();
+        ListNode dummyNode = new ListNode(-1);
+        ListNode curNode = dummyNode;
 
-        while (l1 != null) {
-            list1.add((long) l1.val);
-            l1 = l1.next;
+        ListNode t1 = l1;
+        ListNode t2 = l2;
+
+        int carry = 0;
+
+        while (t1 != null || t2 != null) {
+            int sum = carry;
+
+            if (t1 != null) {
+                sum += t1.val;
+            }
+
+            if (t2 != null) {
+                sum += t2.val;
+            }
+
+            ListNode newNode = new ListNode(sum % 10);
+            carry = sum / 10;
+
+            curNode.next = newNode;
+            curNode = curNode.next;
+
+            if (t1 != null) t1 = t1.next;
+            if (t2 != null) t2 = t2.next;
         }
 
-        while (l2 != null) {
-            list2.add((long) l2.val);
-            l2 = l2.next;
+        if(carry != 0){
+            ListNode newNode = new ListNode(carry);
+            curNode.next = newNode;
         }
 
-        Collections.reverse(list1);
-        Collections.reverse(list2);
-
-        BigInteger list1Digits = findDigitFromList(list1);
-        BigInteger list2Digits = findDigitFromList(list2);
-
-        // long sum = list1Digits + list2Digits;
-        BigInteger sum = list1Digits.add(list2Digits);
-
-        // if (sum == 0)
-        //     return new ListNode(0);
-        if (sum.equals(BigInteger.ZERO))
-            return new ListNode(0);
-
-        return convertDigitToLL(sum);
-    }
-
-    public ListNode convertDigitToLL(BigInteger num) {
-        ListNode temp = new ListNode(-1);
-        ListNode tempHead = temp;
-        while (num.compareTo(BigInteger.ZERO) > 0) {
-            // while (num > 0) {
-            // long remainder = num % 10; // got the last digit
-            // temp.next = new ListNode((int) remainder);
-
-            BigInteger[] divMod = num.divideAndRemainder(BigInteger.TEN);
-            temp.next = new ListNode(divMod[1].intValue());
-
-            // num = num / 10;
-            num = divMod[0];
-            temp = temp.next;
-        }
-
-        return tempHead.next;
-    }
-
-    public BigInteger findDigitFromList(List<Long> temp) {
-        BigInteger digit = BigInteger.ZERO;
-
-        for (Long num : temp) {
-            // digit = digit * 10 + num;
-            digit = digit.multiply(BigInteger.TEN).add(BigInteger.valueOf(num));
-        }
-
-        return digit;
+        return dummyNode.next;
     }
 }
